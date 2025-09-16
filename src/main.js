fada6775f2bedddc6707652c1ab414dc4643cf8c
juao99 <jpsshome@gmail.com>
2025-09-16 09:03:21 -0300

    analytics

--- src/main.js
+++ src/main.js
@@ -11,20 +11,17 @@ import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyB8dFKxuyd8le9NWPgxoR_uJkuXBG92CIY',
-    authDomain: 'portifolio-9db7a.firebaseapp.com',
-    projectId: 'portifolio-9db7a',
-    storageBucket: 'portifolio-9db7a.firebasestorage.app',
-    messagingSenderId: '833891180184',
-    appId: '1:833891180184:web:5fcc80854d86565a7c3aed',
-    measurementId: 'G-Y633F5HHKL'
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-// Inicialize o app do Firebase com um nome único
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
 
-// Crie o app Vue como uma instância separada
 const app = createApp(App);
 app.config.globalProperties.$analytics = analytics;
 
